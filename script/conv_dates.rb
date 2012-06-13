38a74c270132e56d2c2e6b30145fbb2e66902d00
Chetan Vaity <chetanv@gmail.com>
2012-06-13 11:24:04 +0530

    Correcions

--- script/conv_dates.rb
+++ script/conv_dates.rb
@@ -8,12 +8,11 @@
 #
 File.open(ARGV[1], "w:UTF-8:UTF-8") do |outf|
   open(ARGV[0], "r:UTF-8:UTF-8").each_line do |line|
-    arr = line.chomp.split(/t/)
+    arr = line.chomp.split(/\t/)
     next if arr.length != 4
     date_str = arr[2]
     next if date_str.nil?
     d = Date.strptime(date_str, "%Y-%m-%d")
-    outf.puts "#{event_id},#{t}"
-    end
+    outf.puts "#{arr[0]}\t#{arr[1]}\t#{d.jd}\t#{arr[3]}"
   end
 end
