18348d5b205fce266b021a33d773f304cecfc501
Chetan Vaity <chetanv@gmail.com>
2012-06-28 10:39:06 +0530

    * On the Edit form, the date is seen in user-friendly manner instead of the large integer value   * We have used virtual attributes concept for this (http://railscasts.com/episodes/32-time-in-text-field?view=asciicast)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,27 @@
 # encoding: UTF-8
 
+require 'date'
+
 class Event < ActiveRecord::Base
   has_many :tags, :dependent => :destroy
   # Look at http://api.rubyonrails.org/classes/ActiveRecord/NestedAttributes/ClassMethods.html
   accepts_nested_attributes_for :tags, :allow_destroy => true
+
+  # Virtual attribute - date_str for user-friendly date display and entry
+  # See http://railscasts.com/episodes/32-time-in-text-field?view=asciicast
+  # Getter for date_str virtual attribute
+  def date_str
+    Date.jd(self.jd).strftime("%B %d, %Y")
+  end
+
+  # Setter for date_str virtual attribute
+  def date_str=(s)
+    self.jd = Date.parse(s).jd
+  rescue ArgumentError
+    @date_str_invalid = true
+  end
+
+  def validate
+    errors.add(:jd, "is invalid") if @date_str_invalid
+  end
 end
