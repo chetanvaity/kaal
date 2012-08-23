287353af1535d10a0d4f966ff40985e58d9bcffa
Chetan Vaity <chetanv@gmail.com>
2012-08-24 00:16:26 +0530

    Add the @tokens at correct place Also read up more about capistrano - we can do "cap deploy" to update and automatically restart passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,10 @@ after "deploy:restart", "deploy:cleanup"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
+
+# deploy internally does: deploy:update and deploy:restart
+# deploy:update internally does: deploy:update_code, deploy_symlink
+# We have addded more tasks below
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
