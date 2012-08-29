4a4a0e033cef4c09c3ad71cfed52721e9161d35d
Chetan Vaity <chetanv@gmail.com>
2012-08-30 01:59:36 +0530

    New cap task to show maintenance page. Not tested yet.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,6 +50,36 @@ namespace :deploy do
     run "mkdir #{release_path}/public/tmpjson"
   end
 
+  namespace :web do
+    desc <<-DESC
+      Present a maintenance page to visitors. Disables your application's web \
+      interface by writing a "maintenance.html" file to each web server. The \
+      servers must be configured to detect the presence of this file, and if \
+      it is present, always display it instead of performing the request.
+
+      By default, the maintenance page will just say the site is down for \
+      "maintenance", and will be back "shortly", but you can customize the \
+      page by specifying the REASON and UNTIL environment variables:
+
+        $ cap deploy:web:disable \\
+              REASON="a hardware upgrade" \\
+              UNTIL="12pm Central Time"
+
+      Further customization will require that you write your own task.
+    DESC
+    task :disable, :roles => :web do
+      require 'erb'
+        on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+
+      reason = ENV['REASON']
+      deadline = ENV['UNTIL']      
+      template = File.read('app/views/admin/maintenance.html.erb')
+      page = ERB.new(template).result(binding)
+
+        put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+  end
+
 end
 
 # The order here is important
