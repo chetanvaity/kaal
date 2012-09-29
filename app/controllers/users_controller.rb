56eb562ee421c3227cde3e585956fb65af022789
Chetan Vaity <chetanv@gmail.com>
2012-09-29 15:28:05 +0530

    My Content page done - with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def mycontent
-    @timelines = []
-    @events = []
+    @events = Event.where("ownerid = ?", current_user.id).order("jd").page(params[:page]).per(20)
+    @timelines = Timeline.where("owner_id = ?", current_user.id).order("created_at DESC").page(params[:page]).per(12)
   end  
 end
