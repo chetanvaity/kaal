0c0cef45cb208a36ead916c76ca9d8ae8aa9dd4f
Chetan Vaity <chetanv@gmail.com>
2012-10-03 17:33:46 +0530

    Remove extra local variable in timeline query function

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -88,21 +88,11 @@ class TimelinesController < ApplicationController
 
   def search
     @tlquery = params[:tlquery]
-    logger.debug("Search got fired for #{@tlquery}")
-      
-    # We need this local variable for search.
-    local_query_var = params[:tlquery]
+    logger.debug("Search got fired for tlquery=#{@tlquery}")
     
-    
-    @search = Timeline.search() do
-      #
-      # AMOL: Please use local variable here to give search terms. Don't use class variable
-      # like @tlquery in this block. Somehow it results in to *.* result always.
-      # I did not understand the reason why @tlquery did not work properly.
-      # Chetan, see if you can figure it out.
-      #
-      keywords local_query_var, :fields => [:title, :tags, :desc]
-      paginate :page => params[:page], :per_page => NUM_OF_TIMELINES_PER_PAGE
+    @search = Timeline.search() do |q|
+      q.keywords @tlquery, :fields => [:title, :tags, :desc]
+      q.paginate :page => params[:page], :per_page => NUM_OF_TIMELINES_PER_PAGE
     end
     
     #if @search.total == 0
