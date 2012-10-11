43c29ab5034e321bed0335a8ca76977d9f24ab84
amolpotnis <amolpotnis@server>
2012-10-11 10:27:51 +0530

    - Homepage is done from my side. :-)

--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(:version => 20121010162007) do
     t.string  "source",      :limit => 10
     t.text    "desc"
     t.string  "url"
-    t.integer "wiki_id"
     t.integer "ownerid"
+    t.integer "wiki_id"
     t.text    "extra_words"
     t.string  "imgurl"
   end
