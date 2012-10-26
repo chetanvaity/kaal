80ba4e21f3bf5a4766981096b0ea7cde8ced8269
Chetan Vaity <chetanv@gmail.com>
2012-10-26 16:29:31 +0530

    Ask for login on /my page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signing_is_must, only: [:mycontent]
+  
   def mycontent
     @events = Event.where("ownerid = ?", current_user.id).order("jd").page(params[:page]).per(20)
     @timelines = Timeline.where("owner_id = ?", current_user.id).order("created_at DESC").page(params[:page]).per(12)
