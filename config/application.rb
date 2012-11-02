6d494091b5e41fbae426cd34e6aed4bcf3900f88
Chetan Vaity <chetanv@gmail.com>
2012-11-02 13:08:05 +0530

    Fixed #50 SHow activity log in IST

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,8 @@ module Kaal
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Mumbai'
+    config.active_record.default_timezone = 'Mumbai'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -50,5 +51,6 @@ module Kaal
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
   end
 end
