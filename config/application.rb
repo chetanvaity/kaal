d21e54da81716eabdd3712004dad5f53db1530d9
Chetan Vaity <chetanv@gmail.com>
2013-02-17 21:43:57 +0530

    Remove warning about ActiveRecord database timezone

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Kaal
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Mumbai'
-    config.active_record.default_timezone = 'Mumbai'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
